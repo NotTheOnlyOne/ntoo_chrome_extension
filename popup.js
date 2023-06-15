f13fb65333debb5ca356359b49209499bb0afa47
Rowland Mosbergen <mosbergen.r@wehi.edu.au>
2023-06-15 10:04:01 +1000

    Got it working with an alert

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('submitForm').addEventListener('click', function() {
+    // Handle the click event for "Submit to Google Forms" menu item
+    alert('Submitting to Google Forms');
+    // Add your code to submit the form to Google Forms here
+  });
+});
