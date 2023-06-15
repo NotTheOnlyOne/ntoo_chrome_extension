9e698fd69ff3178c447b49d3410601ed8b9ed2bf
Rowland Mosbergen <mosbergen.r@wehi.edu.au>
2023-06-15 10:00:36 +1000

    Got the extension to at least have to menu items

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
 chrome.action.onClicked.addListener(async (tab) => {
-  const [tabId] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tabId.id },
-    files: ['content.js']
-  }, () => {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-    } else {
-      alert("I'm here!");
-    }
-  });
+  alert("Extension clicked!");
 });
