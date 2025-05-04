3b0f35de50c29cdd3b5bba9780f7bd47621a0ebe
Rowland <rowland.h.mosbergen@gmail.com>
2025-05-04 19:47:27 +1000

    Working on nottheonlyone.org

--- popup.js
+++ popup.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('submitForm').addEventListener('click', function() {
-    // Handle the click event for "Submit to Google Forms" menu item
 
     // Retrieve the title and URL of the current website
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const title = tabs[0].title;
       const url = tabs[0].url;
 
-      const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLScqWMQsFNLQZ3sMQue8cG9zFF5gP-soiJcbPE9WNm0dmiLSHA/viewform?entry.759453538=${encodeURIComponent(title)}&entry.1621102160=${encodeURIComponent(url)}`;
+      const formUrl = `https://nottheonlyone.org/add_item?title=${encodeURIComponent(title)}&link=${encodeURIComponent(url)}`;
 
       // Open the Google Form in a new tab
       chrome.tabs.create({ url: formUrl });
